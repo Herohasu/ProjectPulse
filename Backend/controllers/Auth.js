f4555a80f50586645c6a258918b2fd0c29d3f652
DhavalBavda <dhavalbavdas@gmail.com>
2024-07-30 16:03:55 +0530

    ../.

--- Backend/controllers/Auth.js
+++ Backend/controllers/Auth.js
@@ -107,14 +107,14 @@ const requestPasswordReset = async (req,res)=>{
         }
     });
 
-    
+
     const mailOptions = {
         to: "studyai2909@gmail.com",
         from: 'passwordreset@projectpulse.com',
         subject: 'Password Reset',
         text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n` +
               `Please click on the following link, or paste this into your browser to complete the process:\n\n` +
-              `http://localhost:5173//reset-password/${token}\n\n` +
+              `http://localhost:5173/reset-password/${token}\n\n` +
               `If you did not request this, please ignore this email and your password will remain unchanged.\n`
     };
 
