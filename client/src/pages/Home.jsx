72be80a118a305b3432f14109cd2afe2612fed2f
Herohasu <harsh.ajay2017@gmail.com>
2024-08-04 09:45:51 +0530

    topnavbar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,9 +3,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from 'react-router-dom';
 import { Logout, updateUser } from '../redux/AuthSlice';
 import { post } from '../services/ApiEndpoint';
-import { FaBell, FaCalendar, FaHome, FaTasks, FaUser } from 'react-icons/fa';
+import { FaBell, FaCalendar, FaHome, FaTasks, FaUser, FaWindowClose } from 'react-icons/fa';
 import { RiTeamFill } from "react-icons/ri";
-import { IoMenu, IoCloseCircle } from 'react-icons/io5';
+import { FaBarsStaggered } from "react-icons/fa6";
 import './Home.css';
 import UserDashboard from '../components/UserModules/UserDashboard';
 import UserNotification from '../components/UserModules/UserNotification';
@@ -14,11 +14,23 @@ import UserProject from '../components/UserModules/UserProject';
 import UserProfile from '../components/UserModules/UserProfile';
 import UserTeam from '../components/UserModules/UserTeam';
 
+const IconButton = ({ isSidebarOpen, onClick }) => (
+  <button 
+    onClick={onClick} 
+    className="icon-button"
+  >
+    {isSidebarOpen ? 
+      <FaWindowClose size={20} color='#193956' /> : 
+      <FaBarsStaggered size={20} color='#193956' style={{ transform: 'rotate(180deg)'}}/>
+    }
+  </button>
+);
+
 const Home = () => {
   const user = useSelector((state) => state.Auth.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [currentSection, setCurrentSection] = useState('dashboard');
 
   useEffect(() => {
@@ -36,7 +48,6 @@ const Home = () => {
   const handleLogout = async () => {
     try {
       const request = await post('/api/auth/logout');
-      const response = request.data;
       if (request.status === 200) {
         dispatch(Logout());
         navigate('/');
@@ -83,30 +94,24 @@ const Home = () => {
     <>
       {/* Top Navbar */}
       <div className="top-navbar">
+        <IconButton isSidebarOpen={isSidebarOpen} onClick={toggleSidebar} />
         <div className="logo">
           <img src="./logo.jpeg" alt="Project Pulse Logo" className="logo-img" />
-          <span className="project-name">ProjectPulse</span>
+          <span className="project-name"><h3>ProjectPulse</h3></span>
         </div>
-
-        <div className="actions">
+        <div className="center-actions">
           <div className="notifications" onClick={() => handleNavItemClicked('notifications')}>
             <FaBell size={20} />
           </div>
           <div className="calendar" onClick={() => handleNavItemClicked('calendar')}>
             <FaCalendar size={20} />
           </div>
+          <div className="admin-profile">
+            <h3>Welcome</h3>
+            <p>{user && user.name}</p>
+          </div>
         </div>
-
-        <div className="admin-profile">
-          <h3>Welcome</h3>
-          <p>{user && user.name}</p>
-        </div>
-
         <button className="logout-button" onClick={handleLogout}>Logout</button>
-
-        <button className="sidebar-toggle" onClick={toggleSidebar}>
-          {isSidebarOpen ? <IoCloseCircle size={20} /> : <IoMenu size={20} />}
-        </button>
       </div>
 
       {/* Sidebar */}
