ef8cfea02a86963913c69e48dd01e5647e9fbd8d
harsh <harsh.ajay2017@gmail.com>
2024-09-13 14:55:24 +0530

    ..

--- client/src/components/UserModules/UserDashboard.jsx
+++ client/src/components/UserModules/UserDashboard.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { useState,useEffect} from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 const UserDashboard = () => {
-  return (
-    <div>UserDashboard</div>
-  )
-}
+  const [ProjectsData, setProjectsData] = useState([])
+  const user = useSelector((state) => state.Auth.user);
 
-export default UserDashboard
+  useEffect(() => {
+    axios.get(`http://localhost:4000/ShowProjectsByEmail/${user.email}`)
+      .then(result => {
+        setProjectsData(result.data)
+      })
+  })
+   const approvedProjectsCount = ProjectsData.filter((project) => project.Status === 'yes').length;
+ 
+  return (
+    <div>
+      <h2>User Dashboard</h2>
+      <p>Total Approved Projects: {approvedProjectsCount}</p>
+      <p>Project Details </p>
+    </div>
+  );
+};
+
+export default UserDashboard;
